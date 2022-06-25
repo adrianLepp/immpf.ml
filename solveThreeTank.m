49ed216774bb1db49e225f1427e648f41097e02f
adrianLepp <98398878+adrianLepp@users.noreply.github.com>
2022-06-25 11:07:54 +0200

    main: update solveThreeTank

--- solveThreeTank.m
+++ solveThreeTank.m
@@ -1,22 +1,18 @@
-function [x_k,y_k] = solveThreeTank(x_k,parameter)
+function [xK,yK] = solveThreeTank(xK,parameter)
 %solveDreitank: solve equations of three tank system in discrete form for
 %one time step by approximation of Runge Kutta method 4. order
 
+%25.06.2022 Adrian Lepp
+
 %___INPUTS:
-%   x_k         state x for k-1
+%   xK          state x for k-1
 %   parameter   parameters for the system equations, noise sigma , time step dt
 
 %___OUTPUTS:
-%   x_k         state x for k
-
-        u = parameter.u;
-        c13 = parameter.c13;
-        c32 = parameter.c32;
-        cA2 = parameter.cA2;
-        A = parameter.A;
-        g = parameter.g;
-
-        c = parameter.c; %measurement matrix: y=h(x)=c*x
+%   xK          state x for k
+%   yK          measurement y for k
+ 
+        c = parameter.c; % measurement matrix: y=h(x)=c*x
         
         %Noise
         sigmaX = parameter.sigmaX; % process noise
@@ -27,29 +23,31 @@ function [x_k,y_k] = solveThreeTank(x_k,parameter)
 
         % Runge-Kutta 4. order
         for tau = dt : dt : T
-            xtemp = x_k;
-            dx1(1,1) = 1/A*(u-c13*sign(xtemp(1)-xtemp(3))*sqrt(2*g*abs(xtemp(1)-xtemp(3))));
-            dx1(2,1) = 1/A*(c32*sign(xtemp(3)-xtemp(2))*sqrt(2*g*abs(xtemp(3)-xtemp(2)))-cA2*sqrt(2*g*abs(xtemp(2))));
-            dx1(3,1) = 1/A*(c13*sign(xtemp(1)-xtemp(3))*sqrt(2*g*abs(xtemp(1)-xtemp(3)))-c32*sign(xtemp(3)-xtemp(2))*sqrt(2*g*abs(xtemp(3)-xtemp(2))));
-            dx1 = dx1 * dt;
-            xtemp = x_k + dx1 / 2;
-            dx2(1,1) = 1/A*(u-c13*sign(xtemp(1)-xtemp(3))*sqrt(2*g*abs(xtemp(1)-xtemp(3))));
-            dx2(2,1) = 1/A*(c32*sign(xtemp(3)-xtemp(2))*sqrt(2*g*abs(xtemp(3)-xtemp(2)))-cA2*sqrt(2*g*abs(xtemp(2))));
-            dx2(3,1) = 1/A*(c13*sign(xtemp(1)-xtemp(3))*sqrt(2*g*abs(xtemp(1)-xtemp(3)))-c32*sign(xtemp(3)-xtemp(2))*sqrt(2*g*abs(xtemp(3)-xtemp(2))));
-            dx2 = dx2 * dt;
-            xtemp = x_k + dx2 / 2;
-            dx3(1,1) = 1/A*(u-c13*sign(xtemp(1)-xtemp(3))*sqrt(2*g*abs(xtemp(1)-xtemp(3))));
-            dx3(2,1) = 1/A*(c32*sign(xtemp(3)-xtemp(2))*sqrt(2*g*abs(xtemp(3)-xtemp(2)))-cA2*sqrt(2*g*abs(xtemp(2))));
-            dx3(3,1) = 1/A*(c13*sign(xtemp(1)-xtemp(3))*sqrt(2*g*abs(xtemp(1)-xtemp(3)))-c32*sign(xtemp(3)-xtemp(2))*sqrt(2*g*abs(xtemp(3)-xtemp(2))));
-            dx3 = dx3 * dt;
-            xtemp = x_k + dx3;
-            dx4(1,1) = 1/A*(u-c13*sign(xtemp(1)-xtemp(3))*sqrt(2*g*abs(xtemp(1)-xtemp(3))));
-            dx4(2,1) = 1/A*(c32*sign(xtemp(3)-xtemp(2))*sqrt(2*g*abs(xtemp(3)-xtemp(2)))-cA2*sqrt(2*g*abs(xtemp(2))));
-            dx4(3,1) = 1/A*(c13*sign(xtemp(1)-xtemp(3))*sqrt(2*g*abs(xtemp(1)-xtemp(3)))-c32*sign(xtemp(3)-xtemp(2))*sqrt(2*g*abs(xtemp(3)-xtemp(2))));
-            dx4 = dx4 * dt;
+            xtemp = xK;
+            dx1 = solveThreeTankLinear(xtemp, parameter)*dt;
+            xtemp = xK + dx1 / 2;
+            dx2 = solveThreeTankLinear(xtemp, parameter)*dt;
+            xtemp = xK + dx2 / 2;
+            dx3 = solveThreeTankLinear(xtemp, parameter)*dt;
+            xtemp = xK + dx3 / 2;
+            dx4 = solveThreeTankLinear(xtemp, parameter) *dt;
 
-            x_k = x_k + (dx1 + 2 * dx2 + 2 * dx3 + dx4) / 6 + randn(3,1)*sqrt(sigmaX);
-
-            y_k = c * x_k + randn(3,1)*sqrt(sigmaY);
+            xK = xK + (dx1 + 2 * dx2 + 2 * dx3 + dx4) / 6;
         end
+        xK = xK + randn(3,1) * sqrt(sigmaX);
+        yK = c * xK + randn(3,1) * sqrt(sigmaY);
+        
+    function [dx] =  solveThreeTankLinear(x, parameter)
+            u = parameter.u;
+            c13 = parameter.c13;
+            c32 = parameter.c32;
+            cA2 = parameter.cA2;
+            A = parameter.A;
+            g = parameter.g;
+            dx = zeros(3,1);
+            
+            dx(1) = 1/A*(u-c13*sign(x(1)-x(3))*sqrt(2*g*abs(x(1)-x(3))));
+            dx(2) = 1/A*(c32*sign(x(3)-x(2))*sqrt(2*g*abs(x(3)-x(2)))-cA2*sqrt(2*g*abs(x(2))));
+            dx(3) = 1/A*(c13*sign(x(1)-x(3))*sqrt(2*g*abs(x(1)-x(3)))-c32*sign(x(3)-x(2))*sqrt(2*g*abs(x(3)-x(2))));
+    end
 end
